dea2e28c88b49c2c4974a9df6cf8ee2a8fa5a5a3
jdluques <juan.luque@utec.edu.pe>
2025-07-30 12:19:56 -0500

    feat: add short option for all cli commands

--- src/cli.rs
+++ src/cli.rs
@@ -7,18 +7,18 @@ use crate::files::{
 
 #[derive(Parser)]
 pub struct Args {
-    #[arg(long, help="Generate env.example. file based on project's .env file")]
+    #[arg(long="env-example", short='e', help="Generate env.example. file based on project's .env file")]
     pub env_example: bool,
 
     #[arg(long="append", short='a', help="Append to the .gitignore file rather than write/overwrite it")]
     pub append: bool,
 
-    #[arg(long, visible_alias="ignore", num_args = 0.., action = ArgAction::Set, help="Generate .gitignore file")]
+    #[arg(long="gitignore", short='g', visible_alias="ignore", num_args = 0.., action = ArgAction::Set, help="Generate .gitignore file")]
     pub gitignore: Option<Vec<GitIgnoreType>>,
 
-    #[arg(long, help="Generate LICENSE file")]
+    #[arg(long="license", short='l', help="Generate LICENSE file")]
     pub license: Option<LicenseType>,
 
-    #[arg(long, help="Generate README.md file with project name")]
+    #[arg(long="readme", short='r', help="Generate README.md file with project name")]
     pub readme: bool,
 }
